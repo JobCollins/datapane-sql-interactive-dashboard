7d6f14351d9f1ed2229a9775c3f6b2c9a5f94a4d
JobCollins <dulo.collins@gmail.com>
2021-06-23 20:34:13 +0300

    Bug: ValueError, No objects to concatenate
    Datapane seems to have a problem converting a Pandas DataFrame to a Datapane DataTable
    Raised issue on https://github.com/datapane/datapane/issues/120

--- top_tracks.py
+++ top_tracks.py
@@ -1,25 +1,29 @@
 import datapane as dp
 from connect_to_db import *
+
 top_10_tracks_query = '''
-                        SELECT
-                            t.name trackname,
-                            a.title album_title,
-                            ar.name artist,
-                            COUNT(*) as total_purchases,
-                            SUM(il.unit_price) total_cost
-                        FROM track t
-                        JOIN album a on a.album_id = t.album_id
-                        JOIN artist ar on ar.artist_id = a.artist_id
-                        JOIN invoice_line il  on il.track_id = t.track_id
-                        GROUP BY 1
-                        ORDER BY total_purchases desc
-                        LIMIT 10
-                        '''
+SELECT 
+    t.name trackname,
+    a.title album_title,
+    ar.name artist,
+    COUNT(*) as total_purchases,
+    SUM(il.unit_price) total_cost
+FROM track t 
+JOIN album a on a.album_id = t.album_id
+JOIN artist ar on ar.artist_id = a.artist_id
+JOIN invoice_line il on il.track_id = t.track_id
+GROUP BY 1
+ORDER BY total_purchases desc
+LIMIT 10
+'''
 
 top_10_df = run_query(top_10_tracks_query)
+
+# Debugging
+print("Top 10 Tracks DF")
+
+print(top_10_df)
 
 dp.Report(
     dp.DataTable(top_10_df)
 ).publish(name="Music Sales")
-
-
